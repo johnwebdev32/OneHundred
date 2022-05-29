dba09b2d3530fbb329d4751a48c02a6fefe2231e
johnwebdev32 <105568615+johnwebdev32@users.noreply.github.com>
2022-05-29 08:22:00 -0700

    completed One Hundred project
    One Hundred is complete, a few tweaks here and there is needed.

--- js/site.js
+++ js/site.js
@@ -4,21 +4,63 @@ function getValues(){
     //get values from the page
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
-    //call generateNumbers
+    
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
 
-    //call displayNumbers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers
+        displayNumbers(numbers);
+
+    } else {
+        alert("You must enter integers");
+    }
+    
 }
 
 //generate numbers from the startValue to the endValue
 //logic function
-function generateNumbers(){
+function generateNumbers(startValue, endValue){
 
+    let numbers = [];
+
+    //we want to get all numbers from start to end
+    for(let index = startValue; index <= endValue; index++){
+
+        //this will execute in a loop until index = endValue
+        numbers.push(index);   
+
+    }
+
+    return numbers;
 }
 
 //display the numbers and mark even numbers in bold
 //display or view functions
-function displayNumbers(){
+function displayNumbers(numbers){
 
+    let templateRows = ""; 
+
+    for (let index = 0; index < numbers.length; index++) {
+
+        let className = "even";
+
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+        }
+        else{
+            className = "odd";
+        }
+        
+        templateRows += `<tr><td class="${className}" >${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
 
 
