66a93fecb03f0fd1432067a2e0df7caa04137a9e
johnwebdev32 <105568615+johnwebdev32@users.noreply.github.com>
2022-05-28 20:02:25 -0700

    Update site.js
    started js code

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    //call generateNumbers
+
+    //call displayNumbers
 }
+
+//generate numbers from the startValue to the endValue
+//logic function
+function generateNumbers(){
+
+}
+
+//display the numbers and mark even numbers in bold
+//display or view functions
+function displayNumbers(){
+
+}
+
+
