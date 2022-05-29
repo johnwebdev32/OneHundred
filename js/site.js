e9fcb1445c97861d9a4ae0f892f00ea11ffbe051
johnwebdev32 <105568615+johnwebdev32@users.noreply.github.com>
2022-05-29 08:43:18 -0700

    added js code onto the code page
    Added the JS into the code page, comments also.

--- js/site.js
+++ js/site.js
@@ -1,45 +1,47 @@
-//get the values from the Page
-//starts or controller function
 function getValues(){
+
     //get values from the page
+
     let startValue = document.getElementById("startValue").value;
+
     let endValue = document.getElementById("endValue").value;
     
     //parse into Integers
+
     startValue = parseInt(startValue);
+
     endValue = parseInt(endValue);
 
     if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-        //call generateNumbers
+       
         let numbers = generateNumbers(startValue, endValue);
-        //call displayNumbers
+        
         displayNumbers(numbers);
 
     } else {
         alert("You must enter integers");
-    }
-    
+    }  
 }
 
 //generate numbers from the startValue to the endValue
-//logic function
+
 function generateNumbers(startValue, endValue){
 
     let numbers = [];
 
     //we want to get all numbers from start to end
+
     for(let index = startValue; index <= endValue; index++){
 
         //this will execute in a loop until index = endValue
+        
         numbers.push(index);   
-
     }
-
     return numbers;
 }
 
-//display the numbers and mark even numbers in bold
-//display or view functions
+//display the numbers 
+
 function displayNumbers(numbers){
 
     let templateRows = ""; 
